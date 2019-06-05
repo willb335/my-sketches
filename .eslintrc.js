6e8d7d27ec9a42f926f4412db9634bc9dbd2bf61
Will Bashelor <wbashelor@gmail.com>
2019-06-04 22:27:41 -0400

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: { browser: true, node: true },
+  extends: ['react-app', 'airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+    'import/no-extraneous-dependencies': 0
+  }
+};
