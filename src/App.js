3a72a74a5f6ad2aab98e9acd847243421dc734a7
Will Bashelor <wbashelor@gmail.com>
2019-06-04 22:43:28 -0400

    Create table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Table from './Table';
 import '@zendeskgarden/react-tables/dist/styles.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table />
     </div>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,1 +1,46 @@
 /* ----- Rough Table ------ */
+import React, { Component } from 'react';
+import {
+  Table,
+  Caption,
+  Head,
+  HeaderRow,
+  HeaderCell,
+  Body,
+  Row,
+  Cell
+} from '@zendeskgarden/react-tables';
+
+class RoughTable extends Component {
+  render() {
+    return (
+      <Table>
+        <Caption>Your Unsolved Tickets</Caption>
+        <Head>
+          <HeaderRow>
+            <HeaderCell width="25%">Subject</HeaderCell>
+            <HeaderCell width="25%">Requester</HeaderCell>
+            <HeaderCell width="25%">Requested</HeaderCell>
+            <HeaderCell width="25%">Type</HeaderCell>
+          </HeaderRow>
+        </Head>
+        <Body>
+          <Row>
+            <Cell width="25%">Where are my shoes?</Cell>
+            <Cell width="25%">John Smith</Cell>
+            <Cell width="25%">15 minutes ago</Cell>
+            <Cell width="25%">Ticket</Cell>
+          </Row>
+          <Row>
+            <Cell width="25%">I was charged twice!</Cell>
+            <Cell width="25%">Jane Doe</Cell>
+            <Cell width="25%">25 minutes ago</Cell>
+            <Cell width="25%">Call</Cell>
+          </Row>
+        </Body>
+      </Table>
+    );
+  }
+}
+
+export default RoughTable;
