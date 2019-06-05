1659487ee53571369ed66d6eb02037c557cc02b1
Will Bashelor <wbashelor@gmail.com>
2019-06-05 19:53:10 -0400

    Import roughjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Table from './Table';
 import Legend from './Legend';
-import { Square } from './Shapes';
+import Shape from './Shapes';
 
 import '@zendeskgarden/react-tables/dist/styles.css';
 import './App.css';
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Square />
+      <Shape />
       <Legend />
       <Table />
     </div>
--- src/Shapes.js
+++ src/Shapes.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import rough from 'roughjs';
+import rough from 'roughjs/dist/rough.umd';
 
-const roughSquare = ({ squareElement, squareWidth }) => {
-  const rc = rough.svg(squareElement);
-  const square = rc.rectangle(0, 0, squareWidth, squareWidth, {
-    roughness: 0.8,
-    fill: '#b58863',
-    bowing: 2
-  });
-  squareElement.appendChild(square);
-};
-
-export class Square extends Component {
+class Shape extends Component {
   componentDidMount() {
-    roughSquare({ squareElement: this.roughSquare, squareWidth: 90 });
+    console.log('rough', rough);
+    const rc = rough.canvas(this.canvasRef);
+    rc.rectangle(10, 10, 100, 100);
   }
 
   render() {
-    return <div ref={ref => (this.roughSquare = ref)} />;
+    return (
+      <div>
+        <canvas ref={ref => (this.canvasRef = ref)} width="500" height="500" />
+      </div>
+    );
   }
 }
+
+export default Shape;
