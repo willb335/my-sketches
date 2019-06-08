e81ada569628003f75f7cf28b5703175d503cad1
Will Bashelor <wbashelor@gmail.com>
2019-06-08 09:30:35 -0400

    Add more paths

--- src/HandlebarPanel.js
+++ src/HandlebarPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import rough from 'roughjs/dist/rough.umd';
-import { node1Path } from './handlebarPanelPath';
+import { node1Path, node2Path, node3Path } from './handlebarPanelPath';
 
 class Buddha extends React.Component {
   componentDidMount() {
@@ -8,10 +8,14 @@ class Buddha extends React.Component {
     const rc = rough.svg(this.buddhaRef);
 
     const node1 = rc.path(node1Path, { stroke: 'pink', strokeWidth: 1, roughness: 1, bowing: 2 });
-    // const node2 = rc.path(node2Path, { stroke: 'purple', strokeWidth: 1.5, roughness: .5, bowing: 5 });
+    const node2 = rc.path(node2Path, { stroke: 'purple', strokeWidth: 1.5, roughness: .5, bowing: 5 });
+    const node3 = rc.path(node3Path, { stroke: 'blue', strokeWidth: 1.5, roughness: .5, bowing: 5 });
+
 
     this.buddhaRef.appendChild(node1);
-    // this.buddhaRef.appendChild(node2);
+    this.buddhaRef.appendChild(node2);
+    this.buddhaRef.appendChild(node3);
+
   }
 
   render() {
