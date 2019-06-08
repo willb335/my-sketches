19ac62f59c1288aa8ef6dee6df43ce747691d300
Will Bashelor <wbashelor@gmail.com>
2019-06-08 09:49:50 -0400

    Add blueviolet

--- src/HandlebarPanel.js
+++ src/HandlebarPanel.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import rough from 'roughjs/dist/rough.umd';
-import { node1Path, node2Path, node3Path, node4Path } from './handlebarPanelPath';
+import { node1Path, node2Path, node3Path, node4Path, node5Path } from './handlebarPanelPath';
 
 class Buddha extends React.Component {
   componentDidMount() {
     console.log('rough', rough);
     const rc = rough.svg(this.buddhaRef);
 
-    const node1 = rc.path(node1Path, { stroke: 'pink', strokeWidth: 1, roughness: 1, bowing: 2 });
+    const node1 = rc.path(node1Path, { stroke: 'blueviolet', strokeWidth: 3, roughness: .5, bowing: 0 });
     const node2 = rc.path(node2Path, { stroke: 'purple', strokeWidth: 1.5, roughness: .5, bowing: 5 });
     const node3 = rc.path(node3Path, { stroke: 'blue', strokeWidth: 1.5, roughness: .5, bowing: 5 });
     const node4 = rc.path(node4Path, { stroke: 'orange', strokeWidth: .5, roughness: .5, bowing: 2 });
+    const node5 = rc.path(node5Path, { stroke: 'black', strokeWidth: .5, roughness: .5, bowing: 2 });
+
 
 
 
@@ -18,6 +20,8 @@ class Buddha extends React.Component {
     this.buddhaRef.appendChild(node2);
     this.buddhaRef.appendChild(node3);
     this.buddhaRef.appendChild(node4);
+    this.buddhaRef.appendChild(node5);
+
 
 
   }
