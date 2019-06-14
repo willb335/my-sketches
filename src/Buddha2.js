0d12ff19d929f3af3b1a60605868330dbc83579f
Will Bashelor <wbashelor@gmail.com>
2019-06-14 15:18:34 -0400

    Adjust roughness

--- src/Buddha2.js
+++ src/Buddha2.js
@@ -8,7 +8,7 @@ class Buddha extends React.Component {
     const rc = rough.svg(this.buddhaRef);
 
     const node1 = rc.path(node1Path, { stroke: 'pink', strokeWidth: 2.5, roughness: 1.5, bowing: 5 });
-    const node2 = rc.path(node2Path, { stroke: 'darkorchid', strokeWidth: 1.25, roughness: 1.25, bowing: 5 });
+    const node2 = rc.path(node2Path, { stroke: 'darkorchid', strokeWidth: 1.25, roughness: 1, bowing: 3 });
 
     this.buddhaRef.appendChild(node1);
     this.buddhaRef.appendChild(node2);
